c2cf459b05dbee095d94a84eb86bd87f19240caf
Kushagra <kushagr@kushagra.desktop>
2013-09-10 19:49:40 +0530

    fix X-Frame-Options

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module FbBotflip
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.action_dispatch.default_headers[:'X-Frame-Options'] = "ALLOW-FROM https://apps.facebook.com"
+    config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM https://apps.facebook.com"
   end
 end
