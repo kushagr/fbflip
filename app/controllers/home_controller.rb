42e55057777d2822588dca5e901de0efdb688e9d
Kushagra <kushagr@kushagra.desktop>
2013-09-10 23:01:35 +0530

    changes to sessions_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,12 @@
 class HomeController < ApplicationController
 
 	def index
-		# if params[:signed_request]
-		# 	@signed_request = signed_request
-		# 	puts @signed_request.to_s
-		# 	logger.info @signed_request.to_s
-		# 	logger.info @signed_request
-		# 	user = User.from_facebook(@signed_request)
-		# 	session[:current_user] = user.id
-		# end
+		if params[:signed_request]
+			user = User.from_facebook(signed_request)
+			session[:current_user] = user.id
+			# binding.pry
+			#redirect_to searches_index_path
+		end
 	end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,11 @@ class SessionsController < ApplicationController
 
 	def create
 		if params[:signed_request]
-			user = User.from_facebook(@signed_request)
+			user = User.from_facebook(signed_request)
 			session[:current_user] = user.id
 			# binding.pry
+			redirect_to searches_index_path
+		else
 			redirect_to searches_index_path
 		end
 	end
