17dc4d524fabdc5739f90dd61a2d8d4112e61ba9
Kushagra <kushagr@kushagra.desktop>
2013-09-10 22:55:16 +0530

    changes to sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,10 @@ class SessionsController < ApplicationController
 
 	def create
 		if params[:signed_request]
-			@signed_request = signed_request
-			puts @signed_request.to_s
-			logger.info @signed_request.to_s
-			logger.info @signed_request
 			user = User.from_facebook(@signed_request)
 			session[:current_user] = user.id
 			# binding.pry
 			redirect_to searches_index_path
-		else
-			# @facebook_cookies = facebook_cookies
-			# user = User.from_facebook(@facebook_cookies)
-			# session[:current_user] = user.id
-			# # binding.pry
-			 #redirect_to searches_index_path
 		end
 	end
 end
