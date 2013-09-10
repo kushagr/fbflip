bf43be7ff297d889b09eafe62b1c49e1bddea73a
Kushagra <kushagr@kushagra.desktop>
2013-09-10 19:12:01 +0530

    methodfix for POST->GET

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ FbBotflip::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.middleware.use Rack::Facebook::MethodFix 
+
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w( facebook.js )
