5f1840b8b37f605862e0b9effb836cbc69690d8c
Kushagra <kushagr@kushagra.desktop>
2013-09-12 03:54:08 +0530

    changes to home_controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,11 +3,7 @@ class SearchesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    if current_user
-      @user = current_user
-    else
-      @user = nil
-    end
+    
   end
 
 end
