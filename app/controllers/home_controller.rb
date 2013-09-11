f52195074d87719822e1e179b72803117d9c8c0f
Kushagra <kushagr@kushagra.desktop>
2013-09-12 04:03:22 +0530

    changes to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,7 @@
 class HomeController < ApplicationController
 
 	def index
-		# if params[:signed_request]
-		# 	user = User.from_facebook(signed_request)
-		# 	if (user)
-		# 		session[:current_user] = user.id
-		# 	else
-		# 		session[:current_user] = nil
-		# 	end
-		# end
+		
 	end
 
 end
